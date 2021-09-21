6a3633a25e7b8ad19aaf474afbbd74dffb0461eb
Elizabeth <stereoliza.eth>
2021-09-21 10:40:29 -0400

    clean up2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from cache import get_value
+import cache_
 from location import get_response
 import sys
 
 ip = str(sys.argv[1])
 response = get_response(ip)
-get_value(ip, response)
+cache_.get_value(ip, response)
