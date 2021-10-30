a85c1e99bfbb5255ff408aae0fa6c34b1990d401
Elizabeth <stereoliza.eth>
2021-10-30 13:23:28 -0700

    fixed test classes and methods

--- test_cache_strategies.py
+++ test_cache_strategies.py
@@ -2,8 +2,11 @@ from cache_class import MySQLStrategy, RedisStrategy, FileStrategy
 
 
 class Strategy:
-    key = 'foo'
-    value = 'foo2'
+
+    @classmethod
+    def setup_class(cls, key='foo', value='foo2'):
+        cls.key = key
+        cls.value = value
 
     def test_getting_and_setting_data_to_database(self):
         data_before = self.obj.get_data(self.key)
@@ -13,6 +16,10 @@ class Strategy:
         data_after = self.obj.get_data(self.key)
         assert data_after == self.value
 
+    def test_strategy_method_returns_string(self):
+        returned_value = self.obj.get_data(self.key)
+        assert isinstance(returned_value, str)
+
     @classmethod
     def teardown_class(cls):
         cls.obj.delete_data(cls.key)
@@ -20,12 +27,24 @@ class Strategy:
 
 
 class TestMySQLStrategy(Strategy):
-    obj = MySQLStrategy()
+
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        cls.obj = MySQLStrategy()
 
 
 class TestRedisStrategy(Strategy):
-    obj = RedisStrategy()
+
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        cls.obj = RedisStrategy()
 
 
 class TestFileStrategy(Strategy):
-    obj = FileStrategy()
+
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        cls.obj = FileStrategy()
